ab8142a52ace0ef0b52fbff53da7f01a3b1d79e7
Karen <karenkirakosy@gmail.com>
2023-11-07 22:11:28 +0400

    Lecture 10 homework

---
+++ lec_10/main.py
@@ -0,0 +1,36 @@
+import vehicle
+
+class Car(vehicle.Vehicle):
+    def __init__(self, weight, speed):
+        super().__init__(weight)
+        self.speed = speed   
+    def move(self):
+        print("A car moves.")
+
+class Plane(vehicle.Vehicle):
+    def __init__(self, weight, speed, engineCount):
+        super().__init__(weight)
+        self.speed = speed
+        self.engineCount = engineCount
+    def move(self):
+        print("A plane moves.")
+
+class Boat(vehicle.Vehicle):
+    def __init__(self, weight, speed, maxLoad):
+        super().__init__(weight)
+        self.speed = speed
+        self.maxLoad = maxLoad
+    def move(self):
+        print("A boat moves.")
+
+class RaceCar(Car):
+    def __init__(self, weight, speed, horsePower):
+        super().__init__(weight, speed)
+        self.horsePower = horsePower
+    def move(self):
+        print("A race car moves.")
+    def __str__(self):
+        return f"A racecar with weght - {self.weight}, speed - {self.speed}, horsepower - {self.horsePower} runs!"
+
+myRaceCar = RaceCar(250, 350, 600)
+print(myRaceCar)
---
+++ lec_10/vehicle.py
@@ -0,0 +1,7 @@
+class Vehicle:
+    def __init__(self, weight):
+        self.weight = weight
+        print("Vehicle class object created.")
+    
+    def move(self):
+        pass
